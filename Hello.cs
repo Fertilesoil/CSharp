5106ba95bb3c355de0393746c9e034a31a92f95c
Fertilesoil <gasnbloop@gmail.com>
2023-08-22 11:10:18 -0300

    Aula 01 - Hello World

---
+++ Hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+	Console.WriteLine("A quem tenho eu no céu além de Ti \n E não há na Terra quem eu queira mais que a Ti \n");
+	Console.WriteLine("Estou apaixonado \n Desesperado de amor \n");
+	Console.WriteLine(" \n \n");
+    }
+}
