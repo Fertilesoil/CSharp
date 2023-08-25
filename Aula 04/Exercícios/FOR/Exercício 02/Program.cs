d4b07ba998bba6be431f50878cffb59575ad817c
Fertilesoil <gasnbloop@gmail.com>
2023-08-25 19:20:47 -0300

    Aula 04

---
+++ Aula 04/Exercícios/FOR/Exercício 02/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercício_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0, par = 0, impar = 0;
+
+
+
+            for (int contador = 1; contador <= 10; contador++)
+            {
+                Console.WriteLine("Digite os números: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                par++;
+            }
+
+            else
+            {
+                impar++;
+            }
+
+            }
+
+            Console.Clear();
+            Console.WriteLine($"Total de números pares: {par}");
+            Console.WriteLine($"Total de números impares: {impar}");
+        }
+    }
+}
--- Aula 04/Exercícios/WHILE/Exercício 4/Program.cs
+++ Aula 04/Exercícios/WHILE/Exercício 4/Program.cs
@@ -6,7 +6,9 @@ namespace Exercício_4
     {
         static void Main(string[] args)
         {
-            int idade = 0, identidade, pessoadev;
+            int idade = 0, identidade, pessoadev = 0, backEnd = 0, cisTransFrontM = 0, cisTransMobileH = 0,
+                naoBinarieFull = 0;
+            float  total = 1, media = 0;
             string? sn;
 
 
@@ -24,7 +26,8 @@ namespace Exercício_4
                 Console.WriteLine("5 - Homem Trans");
                 Console.WriteLine("6 - Outros");
                 identidade = Convert.ToInt32(Console.ReadLine());
-                
+                Console.Clear();
+
 
                 if (identidade < 1 || identidade > 6)
                 {
@@ -37,26 +40,62 @@ namespace Exercício_4
                 Console.WriteLine("3 - Mobile");
                 Console.WriteLine("4 - FullStack");
                 pessoadev = Convert.ToInt32(Console.ReadLine());
-                
+                Console.Clear();
 
-                if (identidade < 1 || identidade > 4)
+                if (pessoadev < 1 || pessoadev > 4)
                 {
                     break;
                 }
 
+
                 Console.WriteLine("Certo, quer fazer mais uma pesquisa?");
                 Console.WriteLine("[S]           [N]");
                 sn = Console.ReadLine().ToLower();
+                Console.Clear();
 
                 if (sn.Equals("n"))
                 {
                     break;
                 }
 
+                else
+                {
+                    total++;
+                }
+
+            if (pessoadev == 1)
+            {
+                backEnd++;
+            }
+
+            if (identidade == 1 && identidade == 4 && pessoadev == 2)
+            {
+                cisTransFrontM++;
+            }
+
+            if (identidade == 2 && identidade == 5 && pessoadev == 3 && idade > 40)
+            {
+                cisTransMobileH++;
+            }
+
+            if (identidade == 3 && pessoadev == 4 && idade < 30)
+            {
+                naoBinarieFull++; 
+            }
+
+            media = (idade * total) / total;
+
             }
             while (true);
 
 
+
+            Console.WriteLine($"Pessoas desenvolvedoras Back End:                       {backEnd}");
+            Console.WriteLine($"Mulheres Cis e Trans do FrontEnd:                {cisTransFrontM}");
+            Console.WriteLine($"Homes Cis e Trans do Mobile maiores de 40 anos: {cisTransMobileH}");
+            Console.WriteLine($"Pessoas Não Binaries FullStack menores de 30:    {naoBinarieFull}");
+            Console.WriteLine($"Total de pessoas que responderam a pesquisa:              {total}");
+            Console.WriteLine($"Média de idade das pessoas que responderam à pesquisa:    {media}");
         }
     }
 }
